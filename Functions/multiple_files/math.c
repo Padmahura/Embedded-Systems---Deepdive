b9373bdf294804268bea50c71be192b6984855c6
Padmahura <padmahura.innovate@gmail.com>
2024-10-16 19:48:15 +0530

    Create math.c

---
+++ Functions/multiple_files/math.c
@@ -0,0 +1,21 @@
+/*********************************************
+  This file contains all the additional functions given by the user 
+***********************************************/
+
+
+int math_add(int a, int b)
+{
+	return a+b;
+}
+int math_sub(int a, int b)
+{
+	return a-b;
+}
+long long int math_mult(int a, int b)
+{
+	return (long long int)a*b;   //Type casting
+}
+float math_div(int a, int b)
+{
+	return (float)a/b;   // Type casting
+}
