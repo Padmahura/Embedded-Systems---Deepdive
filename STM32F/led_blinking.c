a6977a84687a3d8bb02c891cb9012da4daa2889c
Padmahura <padmahura.innovate@gmail.com>
2024-12-24 22:40:18 +0530

    Update led_blinking.c

--- STM32F/led_blinking.c
+++ STM32F/led_blinking.c
@@ -1,14 +1,18 @@
 /**********************************************************************************
-This is a basic program to turn on an LED(LD2) on the target NUCLEO-F401RE board 
+This is a basic program to blink an LED(LD2) on the target NUCLEO-F401RE board 
 ***********************************************************************************/
 
 #include <stdint.h>
 
+void delay(void){
+	for(uint32_t i=0; i<300000;i++);
+}
+
 int main(void)
 {
-	uint32_t *AHB1ENR = (uint32_t*)0x40023830;  // base addresss = 0x40023800 + offset = 0x30
-	uint32_t *MODER = (uint32_t*)0x40020000;   // base address = 0x40020000 + offset = 0x00
-	uint32_t *ODR =(uint32_t*)0x40020014;   // base address = 0x40020000 + offset = 0x14
+	uint32_t *AHB1ENR = (uint32_t*)0x40023830;
+	uint32_t *MODER = (uint32_t*)0x40020000;
+	uint32_t *ODR =(uint32_t*)0x40020014;
 
 	*AHB1ENR |= 0x01;  // Enable the clock
 
@@ -16,8 +20,12 @@ int main(void)
 	*MODER &= ~(1 << (bitpos+1));
 	*MODER |= (1 << bitpos);  // Enable GPIO mode
 
-	*ODR |= (1 << 5);  // Enable GPIO output
-
 	/* Loop forever */
-	for(;;);
+	for(;;){
+		*ODR |= (1 << 5);  // Enable GPIO output
+		delay();
+		*ODR &= (~(1 << 5));  // Enable GPIO output
+		delay();
+
+	}
 }
